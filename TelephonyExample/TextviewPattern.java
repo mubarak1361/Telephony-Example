a1924dc9a12833179cabfe458d9b1bc22718f862
mubarak1361 <disfrutarz@gmail.com>
2014-09-05 15:43:16 +0530

    changes

--- TelephonyExample/TextviewPattern.java
+++ TelephonyExample/TextviewPattern.java
@@ -4,10 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
-import android.util.Patterns;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.widget.TextView;
 
 public class TextviewPattern extends Activity {
@@ -18,78 +21,41 @@ public class TextviewPattern extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_textview_pattern);
-		
+			
 		text =(TextView)findViewById(R.id.text);
 		
-		String a = "This is a #sample #twitter text of @tom_cruise with a link  http://www.google.com \n\n\n"
+		String a = "This is a #sample #twitter text of @tom_cruise with a link  http://www.google.com \n"
 				+ "\n The @browser will remove extra spaces and extra #lines when the page is #displayed. Any number of lines count"
 				+ "\n\n As #one line, and any number of spaces @count as one #space. \n\n disfrutarz@gmail.com"
-				+ "\n\n          The @browser will remove extra spaces and extra #lines when the page is #displayed. "
+				+ "\n\n  The @browser will remove extra spaces and extra #lines when the page is #displayed. "
 				+ "\n\nAny number of lines count #9o0-9023jdlas-_ "
 				+ "\n\n  #Displays text to the @user and opt"
 				+ "ionally allows them to @edit it. "
 				+ "\n\n       A @TextView is a complete text editor, however the basic class is #configured to not allow editing "
-				+ "\n\n       see @EditText for a          subclass that #configures the text view for #editing. ";
-
+				+ "\n\n       see @EditText for a          subclass that #configures the text view for #editing. ";	
+		 
+		SpannableStringBuilder spannable = new SpannableStringBuilder( a );
+		
 		Pattern mentionPattern = Pattern.compile("(@[A-Za-z0-9_-]+)");
 		Pattern hashtagPattern = Pattern.compile("(#[A-Za-z0-9_-]+)");
-		Pattern urlPattern = Patterns.WEB_URL;
-		Pattern emailPattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-		Pattern newLinePattern = Pattern.compile("\n");
-		Pattern whiteSpacePattern = Pattern.compile("\\s\\s");
 
-		StringBuffer sb = new StringBuffer(a.length());
-		Matcher nl = newLinePattern.matcher(a);
-		
-		while (nl.find()) {
-		    nl.appendReplacement(sb, nl.group(0) + "<br>");
-		}
-		nl.appendTail(sb);
-		
-		Matcher ws = whiteSpacePattern.matcher(sb.toString());
-		sb = new StringBuffer(sb.length());
-		
-		while(ws.find()) {
-			
-			ws.appendReplacement(sb, ws.group(0)+"&nbsp;");
-		}
-		ws.appendTail(sb);
-
-		Matcher o = hashtagPattern.matcher(sb.toString());
-		sb = new StringBuffer(sb.length());
+		Matcher o = hashtagPattern.matcher(spannable);	
 		
 		while (o.find()) {
-		    o.appendReplacement(sb, "<strong><font color=\"#673ab7\">" + o.group(1) + "</font></strong>");
+		   
+			spannable.setSpan(new StyleSpan(Typeface.BOLD), o.start(), o.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		    spannable.setSpan(new ForegroundColorSpan(Color.rgb(103, 58,  183)), o.start(), o.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 		}
-		o.appendTail(sb);
-
-		Matcher n = mentionPattern.matcher(sb.toString());
-		sb = new StringBuffer(sb.length());
+	
+		Matcher n = mentionPattern.matcher(spannable);
 
 		while (n.find()) {
-		    n.appendReplacement(sb, "<strong><font color=\"#00695c\">" + n.group(1) + "</font></strong>");
+			 spannable.setSpan(new StyleSpan(Typeface.BOLD), n.start(), n.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			 spannable.setSpan(new ForegroundColorSpan(Color.rgb(0, 105,  92)), n.start(), n.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 		}
-		n.appendTail(sb);
 
-		Matcher m = urlPattern.matcher(sb.toString());
-		sb = new StringBuffer(sb.length());
-
-		while (m.find()) {
-		    m.appendReplacement(sb, "<strong><font color=\"#0277bd\"><a href =" + m.group(1) +"\">" + m.group(1) + "</a></font></strong>");
-		}
-		m.appendTail(sb);
+		text.setText(spannable);
 		
-		Matcher e =  emailPattern.matcher(sb.toString());
-		sb = new StringBuffer(sb.length());
-		
-		while(e.find()) {
-			e.appendReplacement(sb, "<strong><font color=\"#0277bd\">"+e.group(1)+"</font></strong>");
-		}
-		e.appendTail(sb);
-
-		Log.d("String", String.valueOf(sb));
-		text.setText(Html.fromHtml(sb.toString()));
 	}
 
 }
